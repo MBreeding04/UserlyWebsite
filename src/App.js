2834fcbd3bba7e2d089c0c83ee6a447c29b399ce
Matthew <03mattman@gmail.com>
2023-09-30 04:12:36 -0400

    finished front page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Divider from '@mui/material/Divider';
 import Button from '@mui/material/Button';
-import { CssBaseline, IconButton } from '@mui/material';
+import { Box, CssBaseline, IconButton } from '@mui/material';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import logo from './Assets/Logo/LogoTransparent.png'
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import Typography from '@mui/material/Typography';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 import {
   Route,
   NavLink,
@@ -21,6 +23,11 @@ const Leaguefont = createTheme({
     fontFamily: ['League Spartan', 'sans-serif'].join(",")
   },
 });
+const FiraCode = createTheme({
+  typography: {
+    fontFamily: ['Fira Code', 'monospace'].join(",")
+  },
+});
 
 
 
@@ -32,70 +39,76 @@ const pallete = {
 }
 function App() {
   return (
-    <div className='Main-content'>
-      <HashRouter>
-      <div className='NavBar'>
-        <ThemeProvider theme={Leaguefont}>
-          <CssBaseline />
-          <NavLink to="/F.A.Q">
-          <Button variant="contained" sx={{
-            backgroundColor: pallete.aqua,
-            ':hover': {
-              bgcolor: pallete.darkBlue,
-              color: 'white'
-            },
-            margin: 2,
-            paddingX: 3,
-            minWidth: 125
-          }}> <Typography fontSize={20}>F.A.Q</Typography></Button></NavLink>
-          <NavLink to="/Contact">
-          <Button variant="contained" sx={{
-            backgroundColor: pallete.aqua,
-            ':hover': {
-              bgcolor: pallete.darkBlue,
-              color: 'white'
-            },
-            margin: 2,
-            paddingX: 3,
-            minWidth: 125
-          }}><Typography fontSize={20}>Contact</Typography></Button></NavLink>
-          <NavLink to="/Purchase">
-          <Button variant="contained" sx={{
-            backgroundColor: pallete.aqua,
-            ':hover': {
-              bgcolor: pallete.darkBlue,
-              color: 'white'
-            },
-            margin: 2,
-            paddingX: 3,
-            minWidth: 125
-          }}><Typography fontSize={20}>Purchase</Typography></Button></NavLink>
-        </ThemeProvider>
-        <IconButton>
-          <GitHubIcon fontSize='large'></GitHubIcon>
-        </IconButton>
-        <IconButton>
-          <LinkedInIcon fontSize='large'></LinkedInIcon>
-        </IconButton>
-      </div>
-      <Divider sx={{ width: '95%', borderBottomWidth: 2, bgcolor: '#6C757D' }}></Divider>
-      <div className='clickableLogo'><NavLink to="/"><img src={logo} alt='Userly Logo' className='logo'></img></NavLink></div>
-      <div className='body'>
-        <Divider orientation="horizontal" sx={{
-          marginLeft: 4, borderRightWidth: 2,
-          bgcolor: '#6C757D'
-        }} />
-          <div className="content">
-            <Routes>
-              <Route path='/' element={<Home></Home>}/>
-              <Route path="/F.A.Q" element={<div></div>} />
-              <Route path="/Purchase" element={<div></div>} />
-              <Route path="/Contact" element={<div></div>} />
-            </Routes>
+    <SimpleBar style={{ maxHeight: window.innerHeight }}>
+      <div className='Main-content'>
+        <HashRouter>
+          <div className='NavBar'>
+            <ThemeProvider theme={Leaguefont}>
+              <CssBaseline />
+              <NavLink to="/F.A.Q">
+                <Button variant="contained" sx={{
+                  backgroundColor: pallete.aqua,
+                  ':hover': {
+                    bgcolor: pallete.darkBlue,
+                    color: 'white'
+                  },
+                  margin: 2,
+                  paddingX: 3,
+                  minWidth: 125
+                }}> <Typography fontSize={20}>F.A.Q</Typography></Button></NavLink>
+              <NavLink to="/Contact">
+                <Button variant="contained" sx={{
+                  backgroundColor: pallete.aqua,
+                  ':hover': {
+                    bgcolor: pallete.darkBlue,
+                    color: 'white'
+                  },
+                  margin: 2,
+                  paddingX: 3,
+                  minWidth: 125
+                }}><Typography fontSize={20}>Contact</Typography></Button></NavLink>
+              <NavLink to="/Purchase">
+                <Button variant="contained" sx={{
+                  backgroundColor: pallete.aqua,
+                  ':hover': {
+                    bgcolor: pallete.darkBlue,
+                    color: 'white'
+                  },
+                  margin: 2,
+                  paddingX: 3,
+                  minWidth: 125
+                }}><Typography fontSize={20}>Purchase</Typography></Button></NavLink>
+            </ThemeProvider>
+            <IconButton>
+              <GitHubIcon fontSize='large'></GitHubIcon>
+            </IconButton>
+            <IconButton>
+              <LinkedInIcon fontSize='large'></LinkedInIcon>
+            </IconButton>
           </div>
-      </div>
-      </HashRouter>
-    </div >
+          <Divider sx={{ width: '95%', borderBottomWidth: 2, bgcolor: '#6C757D' }}></Divider>
+          <div className='clickableLogo'><NavLink to="/"><img src={logo} alt='Userly Logo' className='logo'></img></NavLink></div>
+          <div className='body'>
+            <div></div>
+            <div className="content">
+              <Routes>
+                <Route path='/' element={<Home></Home>} />
+                <Route path="/F.A.Q" element={<div></div>} />
+                <Route path="/Purchase" element={<div></div>} />
+                <Route path="/Contact" element={<div></div>} />
+              </Routes>
+            </div>
+          </div>
+
+        </HashRouter>
+        {/* Footer copyright etc. */}<Box sx={{ bgcolor: '#04060A', display:'flex', flexDirection: 'row'}}>
+          <Divider orientation="horizontal" sx={{
+            marginLeft: 4,marginBottom: '2em', borderRightWidth: 2,
+            bgcolor: '#2E3F4C'
+          }} /><ThemeProvider theme={FiraCode}><Typography color='white'sx={{ml: '2em', marginY: '2em'}}>Copyright © Userly 2023. All Rights Reserved</Typography></ThemeProvider>
+        </Box>
+      </div >
+    </SimpleBar>
   );
 }
 
--- src/Modules/home/home.js
+++ src/Modules/home/home.js
@@ -4,7 +4,7 @@ import Button from '@mui/material/Button';
 import { Box } from '@mui/material';
 import ForwardIcon from '@mui/icons-material/Forward';
 import Screenshot from '../../Assets/screenshots/Userly Demo.png'
-
+import Divider from '@mui/material/Divider';
 import './home.css'
 const pallete = {
     aqua: '#4B636D',
@@ -28,55 +28,84 @@ const OnestFont = createTheme({
 export default function Home() {
     return (
         <div className='biggerheaderContainer'>
-            <div className='headerContainer'>
-                <div className='download'>
-                    <Button variant='contained' sx={{
-                        borderRadius: 6,
-                        backgroundColor: pallete.darkBlue,
-                        ':hover': {
-                            bgcolor: '#06080A',
-                            color: 'white'
-                        }, fontSize: 100, width: 300, height: 70
-                    }}><ThemeProvider theme={Leaguefont}>
-                            <Typography>Buy Now</Typography> </ThemeProvider></Button>
+            <div className='dividercontainer'>
+                <Divider orientation="horizontal" sx={{
+                    marginLeft: 4, borderRightWidth: 2,
+                    bgcolor: '#6C757D'
+                }} />
+                <div className='headerContainer'>
+                    <div className='download'>
+                        <Button variant='contained' sx={{
+                            borderRadius: 6,
+                            backgroundColor: pallete.darkBlue,
+                            ':hover': {
+                                bgcolor: '#06080A',
+                                color: 'white'
+                            }, fontSize: 100, width: 300, height: 70
+                        }}><ThemeProvider theme={Leaguefont}>
+                                <Typography>Buy Now</Typography> </ThemeProvider></Button>
+                    </div>
+                    <Box sx={{ backgroundColor: pallete.offprismarine, p: '1em', borderRadius: 2 }}>
+                        <img className='demo' src={Screenshot} alt='Userly Demo'></img>
+                    </Box>
                 </div>
-                <Box sx={{ backgroundColor: pallete.offprismarine, p: 1, borderRadius: 2 }}>
-                    <img className='demo' src={Screenshot} alt='Userly Demo'></img>
-                </Box>
             </div>
-            <div className='carousal'>
-                <ThemeProvider theme={OnestFont}>
+            <div className='dividercontainer'>
+                <Divider orientation="horizontal" sx={{
+                    marginLeft: 4, borderRightWidth: 2,
+                    bgcolor: '#6C757D'
+                }} />
+                <div className='carousal'>
+                    <ThemeProvider theme={OnestFont}>
 
-                    <Box sx={{
-                        p: '1em', backgroundColor: pallete.offWhiteBlue, borderRadius: 2, maxWidth: '14em', display: 'flex',
-                        flexDirection: 'column', justifyContent: 'center', alignItems: 'center', maxHeight: '15em'
-                    }}>
-                        <ThemeProvider theme={Leaguefont}><Typography variant='h4' color='#F5F5F5'> What
-                        </Typography></ThemeProvider><Typography variant="subtitle1" align='center' color='#F5F5F5'>
-                            A local app designed in React, Electron, and MUI to show accounts and corresponding data associated with each account.
-                        </Typography>
-                    </Box >
-                    <ForwardIcon sx={{ mt: '1.5em' }} style={{fontSize: 60}}></ForwardIcon>
-                    <Box sx={{
-                        p: '1em', backgroundColor: pallete.aqua, borderRadius: 2, maxWidth: '14em', display: 'flex',
-                        flexDirection: 'column', justifyContent: 'top', alignItems: 'center', maxHeight: '15em'
-                    }}>
-                        <ThemeProvider theme={Leaguefont}><Typography color='#F5F5F5'variant='h4'> When
-                        </Typography></ThemeProvider><Typography color='#F5F5F5' variant="subtitle1" align='center'>
-                            Established in 2022 in a github repository by two individuals just trying to store their alt accounts.
-                        </Typography>
+                        <Box sx={{
+                            p: '1em', backgroundColor: pallete.offWhiteBlue, borderRadius: 2, maxWidth: '15em', display: 'flex',
+                            flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
+                        }}>
+                            <ThemeProvider theme={Leaguefont}><Typography variant='h3' color='#F5F5F5'> What
+                            </Typography></ThemeProvider><Typography variant="h6" align='center' color='#F5F5F5'>
+                                A local app designed in React, Electron, and MUI to show accounts and corresponding data associated with each account.
+                            </Typography>
+                        </Box >
+                        <ForwardIcon  sx={{ mt: '2em', color:'white' }} style={{ fontSize: 60 }}></ForwardIcon>
+                        <Box sx={{
+                            p: '1em', backgroundColor: pallete.aqua, borderRadius: 2, maxWidth: '15em', display: 'flex',
+                            flexDirection: 'column', justifyContent: 'top', alignItems: 'center',
+                        }}>
+                            <ThemeProvider theme={Leaguefont}><Typography color='#F5F5F5' variant='h3'> When
+                            </Typography></ThemeProvider><Typography color='#F5F5F5' variant="h6" align='center'>
+                                Established in 2022 in a github repository by two individuals just trying to store their alt accounts.
+                            </Typography>
+                        </Box>
+                        <ForwardIcon sx={{ mt: '2em',color:'white' }} style={{ fontSize: 60 }}></ForwardIcon>
+                        <Box sx={{
+                            p: '1em', backgroundColor: pallete.darkBlue, borderRadius: 2, maxWidth: '15em', display: 'flex',
+                            flexDirection: 'column', justifyContent: 'top', alignItems: 'center',
+                        }}>
+                            <ThemeProvider theme={Leaguefont}><Typography color='#F5F5F5' variant='h3'> Why
+                            </Typography></ThemeProvider><Typography color='#F5F5F5' variant="h6" align='center'>
+                                We want to provide a way for users to enjoy multiple accounts and see their data in an easy and accessible way.
+                            </Typography>
+                        </Box>
+                    </ThemeProvider>
+                </div>
+            </div>
+            <div className='lowerThird'>
+
+                <Divider orientation="horizontal" sx={{
+                    marginLeft: 4, borderRightWidth: 2,
+                    bgcolor: '#587488'
+                }} />
+
+                <Box sx={{ display: 'flex', flexDirection: 'column', width: '100%', alignItems: 'center', mb: '5em' }}>
+                    <Box sx={{ display: 'flex', flexDirection: 'column', maxWidth: '80em' }}>
+                        <ThemeProvider theme={Leaguefont}><Typography variant='h2' align='center' sx={{ mt: 8.5, color: '#F5F5F5' }}>The Big Picture</Typography></ThemeProvider>
+                        <ThemeProvider theme={OnestFont}><Typography variant='h6' align='center' sx={{ mt: 8.5, color: '#F5F5F5' }}>We all have been there,
+                            you need an account to play with your friends and you're not sure what account is the correct rank. Is it account X, Y, Z?
+                            We here at Userly are here to solve this issue by providing you with your one stop shop to store all of your accounts and their corresponding data!
+                        </Typography></ThemeProvider>
                     </Box>
-                    <ForwardIcon sx={{ mt: '1.5em' }} style={{fontSize: 60}}></ForwardIcon>
-                    <Box sx={{
-                        p: '1em', backgroundColor: pallete.darkBlue, borderRadius: 2, maxWidth: '14em', display: 'flex',
-                        flexDirection: 'column', justifyContent: 'top', alignItems: 'center', maxHeight: '15em'
-                    }}>
-                        <ThemeProvider theme={Leaguefont}><Typography color='#F5F5F5' variant='h4'> Why
-                        </Typography></ThemeProvider><Typography color='#F5F5F5' variant="subtitle1" align='center'>
-                            We want to provide a way for users to enjoy multiple accounts and see their data in an easy and accessible way.
-                        </Typography>
-                    </Box>
-                </ThemeProvider>
+                </Box>
             </div>
         </div>
     )
